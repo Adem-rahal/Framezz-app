e44541c958c8cb6bd6ec73a2dd2fc4cdc4e553e5
argonaute <adem.rahal01@gmail.com>
2023-03-21 20:38:31 +0400

    Start browser

--- start_browser.sh
+++ start_browser.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-cd ~/Framezz-app/
-pm2 start npm --name "my-app" -- start
+xrandr -o 3
+cd home/framez/Framezz-app/
+npm start
 sleep 5 # wait for server to start
-/usr/bin/firefox -new-window http://localhost:3000
+chromium-browser http://localhost:3000
